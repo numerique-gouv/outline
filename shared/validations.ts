494bd6a3422d816795e165e4dd1bd5b215ef0ecc
Marie <marie.jeammet@beta.gouv.fr>
2024-02-22 17:39:17 +0100

    ✨(backend) add env variable for maxDomains (#2)
    add environnement variable MAX_ALLOWED_DOMAINS to easily increase the amount of allowed emails domains on instance.

--- shared/validations.ts
+++ shared/validations.ts
@@ -1,3 +1,4 @@
+import env from "./env";
 export const AttachmentValidation = {
   /** The limited allowable mime-types for user and team avatars */
   avatarContentTypes: ["image/jpg", "image/jpeg", "image/png"],
@@ -46,8 +47,8 @@ export const PinValidation = {
 };
 
 export const TeamValidation = {
-  /** The maximum number of domains per team */
-  maxDomains: 10,
+  /** The maximum number of domains per instance */
+  maxDomains: env.MAX_ALLOWED_DOMAINS,
 };
 
 export const UserValidation = {
